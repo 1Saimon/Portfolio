f22e39abeb91d87d10de4acc35b828a31710efc5
Saimon Passaura <saimonpassaurabr10@gmail.com>
2022-01-26 16:32:52 -0300

    footer

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scrool-up-btn').addClass("show");
+        }else{
+            $('.scrool-up-btn').removeClass("show");
+        }
+    });
+
+    //slide-up script
+    $('.scrool-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
 
     //ativar menu e navbar
@@ -12,4 +22,25 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // texto animado
+    var typed = new Typed(".typing",{
+        strings: ["Designer","Editor de Vídeo","Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    var typed = new Typed(".typing-2",{
+        strings: ["Designer","Editor de Vídeo","Freelancer"],
+        typeSpeed: 100,
+         backSpeed: 60,
+         loop: true    
+    });
+  
+
+
+
+
 });
+
